0e3e07c53232787ecc763e852c105bd7479098eb
Feli <felinegri5@gmail.com>
2022-04-03 16:33:39 -0300

    Upload

---
+++ Feli.Blazor.MarkdownEditor/MarkdownEditor.razor.cs
@@ -0,0 +1,34 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace Feli.Blazor.MarkdownEditor
+{
+    public partial class MarkdownEditor
+    {
+        [Parameter]
+        public string Content { get; set; }
+
+        [Parameter]
+        public int Height { get; set; } = 400;
+
+        [Parameter]
+        public string InfoText { get; set; } = "Styling with Markdown is supported";
+
+        private string _previewContent = string.Empty;
+
+        public string GetMarkdown()
+        {
+            return Content;
+        }
+
+        public string GetHTML()
+        {
+            return Markdown.ToHtml(Content);
+        }
+
+        private void UpdatePreviewContent()
+        {
+            _previewContent = GetHTML();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Feli.Blazor.MarkdownEditor
+# Blazor MarkdownEditor
 Simple good looking markdown editor for blazor
+
+![Preview](Preview.png)
+
+## Installation
+```
+dotnet add package Feli.Blazor.MarkdownEditor
+```
+
+## Usage
+Component: MarkdownEditor
+
+Parameters
+- Content: The content the editor will work with
+- Height: The height of the editor and preview sections
+- InfoText: Some help text. Default: `Styling with Markdown is supported`
+
+Methods:
+- GetMarkdown: Gets the markdown text that the user wrote
+- GetHTML: Converts the mardown text to html using Markdig
+
+## License
+
+[MIT](https://github.com/01-Feli/Blazor.MarkdownEditor/blob/main/README.md)
